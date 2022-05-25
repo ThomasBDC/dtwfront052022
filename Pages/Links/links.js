a82db8fc8eef1fa1f8bc1ae20915a48309b81c05
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-05-25 20:14:30 +0200

    Ajout de la recherche

--- Pages/Links/links.js
+++ Pages/Links/links.js
@@ -1,8 +1,10 @@
 const urlApi = 'https://dtw.azurewebsites.net/api'
 const urlApiLinks = urlApi + '/links';
 let currentPage = 1;
-//Juste pour tester l'appel AJAX
+let isSearchMode = false;
+
 function getLinks(perPage = 10, page =1){
+    isSearchMode = false;
     const headers = new Headers();
     headers.append("Content-Type", "application/json")
 
@@ -47,7 +49,14 @@ function getLinks(perPage = 10, page =1){
                 `;
 
             });
-            document.getElementById("linksContainer").innerHTML += myHtml;
+            if(page == 1){
+                //Premier affichage, on vide le contenu de la div
+                document.getElementById("linksContainer").innerHTML = myHtml;
+            }
+            else{
+                //étapes de pagination, on ajoute le contenu en s
+                document.getElementById("linksContainer").innerHTML += myHtml;
+            }
         })
         .catch(error =>{
             alert(error);
@@ -59,6 +68,81 @@ function paginate(){
     getLinks(15,currentPage)
 }
 
+function searchLinks(){
+    isSearchMode = true;
+    currentPage = 1;
+
+    const search = document.getElementById("searchBarInput").value;
+    
+    if(search == '' || search == undefined){
+        getLinks(15, currentPage);
+    }
+    else{
+        const headers = new Headers();
+        headers.append("Content-Type", "application/json")
+    
+        const init = {
+            method: 'GET', 
+            headers: headers
+        };
+    
+        //https://dtw.azurewebsites.net/api/links/search?search=laur
+        const urlRequete = urlApiLinks + '/search?search='+search;
+    
+        fetch(urlRequete, init)
+            .then(response => {
+                return response.json();
+            })
+            .then(response => {
+                //Déclare une chaine de caractères
+                let myHtml = '';
+                
+                //Pour chaque lien que je reçoit de ma requête ajax
+                response.forEach(element => {
+                    /*Je crée un élément en html, qui corresponds 
+                    à l'affichage de mon lien*/
+    
+                myHtml +=
+                    `
+                    <div class="cardLinks">
+                        <div class="card h-100">
+                            <img src="https://picsum.photos/200/100" class="card-img-top" alt="image">
+                            <div class="card-body">
+                                <h5 class="card-title">${element.title}</h5>
+                                <p class="card-text">
+                                ${element.description}
+                                </p>
+                                <a href="${element.link}" class="btn btn-primary">
+                                    Go !   
+                                </a>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">By ${element.author.surName} ${element.author.foreName}</small>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+    
+                });
+                if(myHtml != ''){
+                    document.getElementById("linksContainer").innerHTML = myHtml;
+                }
+                else{
+                    document.getElementById("linksContainer").innerHTML = `<span class="text-danger">Fin de l'affichage des résultats`;
+                }
+            })
+            .catch(error =>{
+                alert(error);
+            });
+    }
+
+    
+}
+
+
+function addLink(){
+    alert("ajouter le lien");
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     getLinks(15,1);
@@ -71,9 +155,11 @@ document.addEventListener('DOMContentLoaded', function () {
             clientHeight//La hauteur de mon écran
         } = document.documentElement;
     
-        if (scrollTop + clientHeight >= scrollHeight -5 ) {
-            //action à faire pour le scroll infini
-            paginate();
+        if(!isSearchMode){
+            if (scrollTop + clientHeight >= scrollHeight -5 ) {
+                //action à faire pour le scroll infini
+                paginate();
+            }
         }
     });
 
